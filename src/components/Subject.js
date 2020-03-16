87513d2d4f99a7b1a8846a870246365fc69d9834
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-03-16 16:12:16 +0600

    add skeleton rendering

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,58 +1,68 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Skeleton from '@material-ui/lab/Skeleton';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core';
+import styled from 'styled-components';
 
-const Subject = ({ subject: { start, end, title, cabinet, teachers }, isNow, ...props }) => {
-  const { classes } = props;
+const SubjectContainer = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  margin: 8px;
+  padding: 16px;
+  border-radius: 4px;
+  box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2),
+              0px 1px 1px 0px rgba(0,0,0,0.14),
+              0px 1px 3px 0px rgba(0,0,0,0.12);
+  background-color: #424242;
+  ${props => props.current ? `
+    &::before {
+    content: '';
+    position: absolute;
+    display: block;
+    height: 100%;
+    width: 4px;
+    top: 0;
+    left: 0;
+    background-color: #aa00ff;
+  }` : ''}
+`;
 
-  return (
-    <Paper className={isNow ? `${classes.subject} ${classes.subjectActive}` : `${classes.subject}`}>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item>
-          <Typography variant="subtitle2">{start}</Typography>
-          <Typography variant="subtitle2">{end}</Typography>
-        </Grid>
-        <Grid item className={classes.info}>
-          <Typography variant="h6" className={classes.name}>
-            {title}
-          </Typography>
-          <Typography variant="subtitle2">{cabinet}</Typography>
-          <Typography variant="subtitle2">{teachers}</Typography>
-        </Grid>
-      </Grid>
-    </Paper>
+const SubjectTime = styled.div``;
+
+const SubjectInfo = styled.div`
+  margin-left: 16px;
+`;
+
+const Subject = props => {
+  const { subject, isCurrent } = props;
+
+  return subject ? (
+    <SubjectContainer current={isCurrent}>
+      <SubjectTime>
+        <Typography variant="subtitle2">{subject.start}</Typography>
+        <Typography variant="subtitle2">{subject.end}</Typography>
+      </SubjectTime>
+      <SubjectInfo>
+        <Typography variant="h6">
+          {subject.title}
+        </Typography>
+        <Typography variant="subtitle2">{subject.cabinet}</Typography>
+        <Typography variant="subtitle2">{subject.teachers}</Typography>
+      </SubjectInfo>
+    </SubjectContainer>
+  ) : (
+    <SubjectContainer>
+      <SubjectTime>
+        <Skeleton width={36} />
+        <Skeleton width={36} />
+      </SubjectTime>
+      <SubjectInfo>
+        <Skeleton width={190} />
+        <Skeleton width={40} />
+        <Skeleton width={160} />
+      </SubjectInfo>
+    </SubjectContainer>
   );
 };
 
-const styles = theme => ({
-  subject: {
-    display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
-    padding: theme.spacing(2),
-    overflowX: 'hidden',
-    margin: theme.spacing(1),
-  },
-  subjectActive: {
-    '&::before': {
-      content: '""',
-      position: 'absolute',
-      display: 'block',
-      height: '100%',
-      width: 4,
-      top: 0,
-      left: 0,
-      backgroundColor: theme.palette.primary.main,
-    },
-  },
-  info: {
-    maxWidth: '70vw',
-  },
-  name: {
-    wordWrap: 'break-word',
-  },
-});
-
-export default withStyles(styles)(Subject);
+export default Subject;
--- src/components/SubjectsList.js
+++ src/components/SubjectsList.js
@@ -1,35 +1,29 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core';
 import { inTimeRange, isToday } from '../utils/Time';
 import Subject from './Subject';
 
-const SubjectList = ({ subjects, index, ...props }) => {
-  const { classes } = props;
+const SubjectsSkeleton = () => {
+  const result = [];
+  for (let i = 0; i < 8; i++) {
+    result.push(<Subject key={i} />);
+  }
+  return result;
+};
+
+const SubjectList = props => {
+  const { subjects, index } = props;
 
   return subjects.length === 0 ? (
-    <span className={classes.empty}>Пар нет</span>
+    <SubjectsSkeleton />
   ) : (
     <>
-      {subjects.map(subject => {
-        return (
-          <Subject
-            key={subject.id}
-            subject={subject}
-            isNow={inTimeRange(subject.start, subject.end) && isToday(index + 1)} />
-        );
-      })}
+      {subjects.map(subject =>
+        <Subject
+          key={subject.id}
+          subject={subject}
+          isCurrent={inTimeRange(subject.start, subject.end) && isToday(index + 1)} />)}
     </>
   );
 };
 
-const styles = theme => ({
-  empty: {
-    display: 'block',
-    marginTop: theme.spacing(3),
-    textAlign: 'center',
-    fontSize: 18,
-    color: 'rgba(255, 255, 255, 0.68)',
-  },
-});
-
-export default withStyles(styles)(SubjectList);
+export default SubjectList;
