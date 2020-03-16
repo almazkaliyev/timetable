f9fc2b23f71ba26db2e302dcad29933611f1657c
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-03-16 13:15:12 +0600

    add webpack integration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['@babel/polyfill', './src/index.js'],
+    vendor: ['react', 'react-dom'],
+  },
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: '[name].[hash].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: 'index.html',
+      template: './public/index.html'
+    }),
+  ],
+  devServer: {
+    port: 8000,
+    contentBase: 'static',
+    historyApiFallback: true,
+  },
+  devtool: 'source-map',
+};
