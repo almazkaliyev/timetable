5c1f830da74d7551b101b7b547b9469090806bd4
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-05-06 11:30:08 +0600

    split global styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './index.css';
+
 document.body.className = localStorage.getItem('mode') || 'light';
 
 ReactDOM.render(
